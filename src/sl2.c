1b52ce14189784157104b2b824e940c91f7ff46b
Sam Rijs <srijs@airpost.net>
2015-01-07 18:57:44 +0100

    sl2: add specialised A and B multiplication

--- src/sl2.c
+++ src/sl2.c
@@ -1,5 +1,29 @@
 #include "sl2.h"
 
+void sl2_mul_a(sl2_t b, sl2_t c) {
+  // A: {00 = 10, 01 = 01, 10 = 01, 11 = 00}
+  c[0][0] = gf2p127_add(gf2p127_mul_10(b[0][0]),
+                        gf2p127_mul_01(b[1][0]));
+  c[0][1] = gf2p127_add(gf2p127_mul_10(b[0][1]),
+                        gf2p127_mul_01(b[1][1]));
+  c[1][0] = gf2p127_add(gf2p127_mul_01(b[0][0]),
+                        gf2p127_mul_00(b[1][0]));
+  c[1][1] = gf2p127_add(gf2p127_mul_01(b[0][1]),
+                        gf2p127_mul_00(b[1][1]));
+};
+
+void sl2_mul_b(sl2_t a, sl2_t c) {
+  // B: {00 = 10, 01 = 11, 10 = 01, 11 = 01}
+  c[0][0] = gf2p127_add(gf2p127_mul_10(a[0][0]),
+                        gf2p127_mul_11(a[1][0]));
+  c[0][1] = gf2p127_add(gf2p127_mul_10(a[0][1]),
+                        gf2p127_mul_11(a[1][1]));
+  c[1][0] = gf2p127_add(gf2p127_mul_01(a[0][0]),
+                        gf2p127_mul_01(a[1][0]));
+  c[1][1] = gf2p127_add(gf2p127_mul_01(a[0][1]),
+                        gf2p127_mul_01(a[1][1]));
+};
+
 void sl2_mul(sl2_t a, sl2_t b, sl2_t c) {
   // Strassen algorithm
   gf2p127_t m0, m1, m2, m3, m4, m5, m6;
