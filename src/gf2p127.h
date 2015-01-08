de94bf622c2e9373b2d0c067837302c000020d3e
Sam Rijs <srijs@airpost.net>
2015-01-08 22:40:00 +0100

    sl2: bit multiplication faster than byte for the first time

--- src/gf2p127.h
+++ src/gf2p127.h
@@ -55,9 +55,8 @@ gf2p127_t gf2p127_mul_10(const gf2p127_t a) {
   gf2p127_t c = _mm_or_si128(sl, _mm_slli_si128(sr, 8));
   // Check for a x^127 overflow, and add the polynom.
   gf2p127_t over = _mm_srli_epi64(sl, 63);
-  gf2p127_t x127 = _mm_slli_epi64(over, 63);
-  gf2p127_t x127x63 = _mm_shuffle_epi32(x127, _MM_SHUFFLE(3, 2, 3, 2));
-  gf2p127_t one = _mm_shuffle_epi32(over, _MM_SHUFFLE(3, 3, 3, 2));
+  gf2p127_t x127x63 = _mm_slli_epi64(_mm_unpackhi_epi64(over, over), 63);
+  gf2p127_t one = _mm_unpackhi_epi64(over, _mm_setzero_si128());
   return _mm_xor_si128(_mm_xor_si128(c, x127x63), one);
 }
 
