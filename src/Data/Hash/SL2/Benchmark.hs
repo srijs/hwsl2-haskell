03c2394da6bea2df90d5e404723b22eb50cf63a0
Sam Rijs <srijs@airpost.net>
2015-02-21 19:11:57 +1100

    Benchmark: add parallel case

--- src/Data/Hash/SL2/Benchmark.hs
+++ src/Data/Hash/SL2/Benchmark.hs
@@ -1,6 +1,9 @@
 import Data.Hash.SL2
 
 import Data.Monoid
+import Data.List (foldl')
+
+import Control.Parallel.Strategies
 
 import qualified Data.ByteString as B
 import qualified Crypto.Hash.SHA1 as SHA1
@@ -12,7 +15,8 @@ bs2M = B.pack $ take (2 * 1024 * 1024) $ cycle [0..255]
 bs4M = B.pack $ take (4 * 1024 * 1024) $ cycle [0..255]
 
 main = defaultMain
-  [ bench "hwsl2 append 1MB" $ whnf (mempty <+) bs1M
-  , bench "hwsl2 prepend 1MB" $ whnf (+> mempty) bs1M
-  , bench "sha1 1MB" $ whnf SHA1.hash bs1M
+  [ bench "hwsl2 append" $ whnf (mempty <+) bs4M
+  , bench "hwsl2 prepend" $ whnf (+> mempty) bs4M
+  , bench "hwsl2 append parallel" $ whnf (mconcat . (parMap rpar hash)) [bs1M, bs1M, bs1M, bs1M]
+  , bench "sha1" $ whnf SHA1.hash bs4M
   ]
