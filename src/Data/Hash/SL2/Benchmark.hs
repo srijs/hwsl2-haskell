eae3191f8a14dcfc4cdc200172e967dd502fe62f
Sam Rijs <srijs@airpost.net>
2015-03-01 23:23:02 +1100

    Benchmark: fix typo

--- src/Data/Hash/SL2/Benchmark.hs
+++ src/Data/Hash/SL2/Benchmark.hs
@@ -14,8 +14,8 @@ bs2M = B.pack $ take (2 * 1024 * 1024) $ cycle [0..255]
 bs4M = B.pack $ take (4 * 1024 * 1024) $ cycle [0..255]
 
 main = defaultMain
-  [ bench "hwsl2 append" $ whnf (append union) bs4M
-  , bench "hwsl2 prepend" $ whnf (flip prepend union) bs4M
+  [ bench "hwsl2 append" $ whnf (append unit) bs4M
+  , bench "hwsl2 prepend" $ whnf (flip prepend unit) bs4M
   , bench "hwsl2 append parallel" $ whnf (concatAll . (parMap rpar hash)) [bs1M, bs1M, bs1M, bs1M]
   , bench "sha256" $ whnf SHA256.hash bs4M
   ]
